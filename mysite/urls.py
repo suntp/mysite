b72b9f2835b8e385b584aa3e162aa66f11a1a11a
suntp <605682931@qq.com>
2017-01-25 21:48:53 -0800

    add files

---
+++ mysite/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+from mysite.views import hello, current_datetime, hours_ahead
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url('^hello/$', hello),
+    ('^time/$', current_datetime),
+    (r'^time/plus/(\d{1,2})/$', hours_ahead),
+)
